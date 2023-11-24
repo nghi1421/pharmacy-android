8c9e7c07c8e058a54d3fae83f6b305120b3a2962
Nguyen Thanh Nghi <thanhnghi1421@gmail.com>
2023-11-25 00:04:19 +0700

    update sign up screen & form text input

---
+++ components/FomTextInput.tsx
@@ -0,0 +1,45 @@
+import { Controller } from "react-hook-form"
+import { Text, TextInput, View } from "react-native"
+import Animated, { FadeInDown } from "react-native-reanimated"
+
+export interface FormInputTextProps {
+    name: string
+    placeholder: string
+    control: any
+}
+
+export const FormTextInput: React.FC<FormInputTextProps> = ({
+    control, 
+    name,
+    placeholder,
+}) => {
+    return (
+        <Animated.View 
+            entering={FadeInDown.duration(1000).springify()} 
+            className="w-full">
+            <Controller
+                name={name}
+                control={control}
+                render={({
+                    field: { onChange, value },
+                    fieldState: { error },
+                }) => (
+                    <View>
+                        <TextInput
+                            className='bg-black/5 p-5 rounded-2xl'
+                            keyboardType="phone-pad"
+                            placeholder={placeholder}
+                            placeholderTextColor={'gray'}
+                            value={value}
+                            onChangeText={onChange}
+                        />
+                        {
+                            error &&
+                            <Text className='mt-2 text-red-600 font-semibold'>{ error.message }</Text>
+                        }
+                    </View>
+                )}
+            />
+        </Animated.View>
+    )
+}
--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,10 +1,22 @@
-import { View, Text, TextInput, TouchableOpacity, Alert, StyleSheet, TextInputComponent } from 'react-native'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  TextInputKeyPressEventData,
+  Image
+} from 'react-native'
 import React, { useEffect, useRef, useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { useNavigation } from '@react-navigation/native'
 import Animated, { FadeInDown, FadeInUp } from 'react-native-reanimated';
-import { Controller, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { useVerifyPhoneNumber } from '../hooks/authenticationHook';
+import { yupResolver } from "@hookform/resolvers/yup";
+import yup from '../utils/yup';
+import { FormTextInput } from '../components/FomTextInput';
+import * as Yup from 'yup';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 export interface VerifyPhoneNumberForm {
@@ -15,165 +27,269 @@ interface TextInputProp {
     focus: () => void
 }
 
+export interface CreateCustomerForm {
+  username: string
+  password: string
+  confirmationPassword: string
+  name: string;
+  phoneNumber: string;
+  address: string;
+  gender: string;
+}
+
+const defaultValues = {
+  username: "",
+  password: "",
+  confirmationPassword: "",
+  name: "",
+  phoneNumber: "",
+  gender: '1',
+  address: ''
+};
+
+//@ts-ignore
+const phoneNumberValidate: Yup.ObjectSchema<VerifyPhoneNumberForm> = yup.object({
+    phoneNumber: yup
+        .string()
+        .required('Số điện thoại bắt buộc.')
+        //@ts-ignore
+        .phoneNumber('Số điện thoại không hợp lệ.'),
+})
+
+//@ts-ignore
+const customerFormValidate: Yup.ObjectSchema<CreateCustomerForm> = yup.object({
+    name: yup
+        .string()
+        .required('Họ và tên bắt buộc.')
+        .max(255),
+    phoneNumber: yup
+        .string()
+        .required('Số điện thoại bắt buộc.')
+        //@ts-ignore
+        .phoneNumber('Số điện thoại không hợp lệ.'),
+    username: yup
+        .string()
+        .required('Tên đăng nhập bắt buộc.')
+        .max(255),
+    password: yup
+        .string()
+        .required('Mật khẩu bắt buộc.')
+        .max(255),
+    confirmationPassword: yup
+        .string()
+        .required('Xác nhận mật khẩu bắt buộc.')
+        .oneOf([Yup.ref('password'), ''], 'Xác nhận mật khẩu không khớp.')
+        .max(255),
+})
+
 export default function SignupScreen() {
-    const navigation = useNavigation();
-    const { control, handleSubmit } = useForm<VerifyPhoneNumberForm>()
-    const verifyPhoneNumber = useVerifyPhoneNumber()
-    const [otp, setOtp] = useState(['', '', '', '']);
-    const inputRefs = useRef<TextInputProp[]>(Array(otp.length).fill({}));
+  const navigation = useNavigation();
+  const {
+    control: phoneNumberControl,
+    handleSubmit: handleSubmitPhoneNumber,
+  } = useForm<VerifyPhoneNumberForm>({
+    defaultValues: { phoneNumber: '' },
+    resolver: yupResolver(phoneNumberValidate)
+  })
+
+  const {
+    control: customerControl,
+    handleSubmit: handleSubmitCustomer,
+  } = useForm<CreateCustomerForm>({
+    defaultValues: defaultValues,
+    resolver: yupResolver(customerFormValidate)
+  })
+
+  const verifyPhoneNumber = useVerifyPhoneNumber()
+  const [otp, setOtp] = useState(['', '', '', '', '', '']);
+  const inputRefs = useRef<TextInputProp[]>(Array(otp.length).fill({}));
+  const [checkOtp, setCheckOtp] = useState<string>('')
+  const [isVeriry, setIsVeriry] = useState<boolean>(false)
+  const onSubmit = async (data: VerifyPhoneNumberForm) => { 
+    verifyPhoneNumber.mutate(data)
+  }
+
+  const handleOtpChange = (index: number, value: string) => {
+      const newOtp = [...otp];
+      newOtp[index] = value;
+      setOtp(newOtp);
+  };
     
-    const onSubmit = async (data: VerifyPhoneNumberForm) => { 
-        verifyPhoneNumber.mutate(data)
-        // const otp = await getOtp();
-        // console.log(otp)
+  const handleKeyPress = (event: TextInputKeyPressEventData, index: number) => {
+    if (event.key === 'Backspace') {
+      if (index - 1 >=0 && inputRefs.current[index - 1]) {
+        inputRefs.current[index - 1].focus();
+      }
     }
+    else {
+      if (index < otp.length - 1 && inputRefs.current[index + 1]) {
+          inputRefs.current[index + 1].focus();
+      }
+    }
+  }
 
-     const handleOtpChange = (index: number, value: string) => {
-        const newOtp = [...otp];
-        newOtp[index] = value;
-        setOtp(newOtp);
-         
-        if (value && index < otp.length - 1 && inputRefs.current[index + 1]) {
-            inputRefs.current[index + 1].focus();
-        }
-    };
-    
-    useEffect(() => {
+  const checkVerify = () => {
+    if (otp.join('') === checkOtp) {
+      setIsVeriry(true)
+    }
+    else {
+      
+    }
+  }
 
-    }, [verifyPhoneNumber.data])
-    // console.log(getOtp())
+  useEffect(() => {
+    setCheckOtp(verifyPhoneNumber.data)
+  }, [verifyPhoneNumber.data])
+
   return (
+  <KeyboardAwareScrollView
+    extraScrollHeight={50}
+    enableOnAndroid
+    className='bg-white'
+  >
     <View className="bg-white h-full w-full">
-      <StatusBar style="light" />
-      {/* <Image className="h-full w-full absolute" source={require('../assets/images/background.png')} />
+    <StatusBar style="light" />
+     <View className="h-10 w-full bg-sky-500" />
 
-      <View className="flex-row justify-around w-full absolute">
-        <Animated.Image 
-            entering={FadeInUp.delay(200).duration(1000).springify()} 
-            source={require('../assets/images/light.png')} 
-            className="h-[225] w-[90]"
-        />
-        <Animated.Image 
-            entering={FadeInUp.delay(400).duration(1000).springify()} 
-            source={require('../assets/images/light.png')} 
-            className="h-[160] w-[65] opacity-75" 
-        />
-      </View> */}
+    <View  className="w-full flex justify-around pt-10">
+      <View className="flex items-center">
+          <Animated.Text 
+              entering={FadeInUp.duration(1000).springify()} 
+              className="text-sky-400 font-bold tracking-wider text-5xl mb-10">
+                  Đăng kí
+          </Animated.Text>
+      </View>
 
-      <View  className="h-full w-full flex justify-around pt-48">
-        <View className="flex items-center">
-            <Animated.Text 
-                entering={FadeInUp.duration(1000).springify()} 
-                className="text-sky-400 font-bold tracking-wider text-5xl">
-                    Đăng kí
-            </Animated.Text>
-        </View>
-
-            <View className="flex items-center mx-5 space-y-4">
-                
-
-                <View style={styles.otpContainer}>
-                    {otp.map((digit, index) => (
-                        <TextInput
-                            ref={(ref) => (inputRefs.current[index] = ref as TextInputProp)}
-                            key={index}
-                            style={styles.input}
-                            maxLength={1}
-                            keyboardType="numeric"
-                            value={digit}
-                            onChangeText={(value) => handleOtpChange(index, value)}
-                        />
-                    ))}
-                </View>
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Verify</Text>
-                </TouchableOpacity>
+      <View className="flex items-center mx-5 space-y-4">
+        {
+          checkOtp
+            ?
+            isVeriry
+              ?
+              <>
+                <Animated.View 
+                  entering={FadeInDown.duration(1000).springify()} 
+                  className="p-2 rounded-2xl w-full">
+                   <FormTextInput
+                    name='username'
+                    placeholder='Tên đăng nhập'
+                    control={customerControl}
+                  />
+                </Animated.View>
+               
+                <Animated.View 
+                  entering={FadeInDown.duration(1000).springify()} 
+                  className="p-2 rounded-2xl w-full"
+                >
+                  <FormTextInput
+                    name='password'
+                    placeholder='Mật khẩu'
+                    control={customerControl}
+                  />
+                </Animated.View>
                   
-            <Animated.View 
-                entering={FadeInDown.duration(1000).springify()} 
-                className="bg-black/5 p-5 rounded-2xl w-full">
-                <Controller
-                            name='phoneNumber'
-                            control={control}
-                            render={({
-                                field: { onChange, value },
-                                fieldState: { error },
-                            }) => (
-                                <TextInput
-                                    keyboardType="phone-pad"
-                                    placeholder="Số điện thoại"
-                                    placeholderTextColor={'gray'}
-                                    value={value}
-                                    onChangeText={onChange}
-                                />
-                            // <TextField
-                            //     size='small'
-                            //     type='text'
-                            //     helperText={error ? error.message : null}
-                            //     error={!!error}
-                            //     onChange={onChange}
-                            //     onBlur={() => {
-                            //         searchCustomer.mutate(watch('phoneNumber'))
-                            //     }
-                            //     }
-                            //     value={value}
-                            //     fullWidth
-                            //     label="Số điện thoại khách hàng"
-                            //     variant="outlined"
-                            //     placeholder='Nhập số điện thoại khách hàng'
-                            // />
-                            )}
-                        />
-                
-            </Animated.View>
+                <Animated.View 
+                  entering={FadeInDown.duration(1000).springify()} 
+                  className="p-2 rounded-2xl w-full"
+                >
+                  <FormTextInput
+                    name='confirmationPassword'
+                    placeholder='Xác nhận mật khẩu'
+                    control={customerControl}
+                  />
+                </Animated.View>
+                  
+                <Animated.View 
+                  entering={FadeInDown.duration(1000).springify()} 
+                  className="p-2 rounded-2xl w-full"
+                >
+                  <FormTextInput
+                    name='name'
+                    placeholder='Họ và tên'
+                    control={customerControl}
+                  />
+                </Animated.View>
+                  
+                <Animated.View 
+                  entering={FadeInDown.duration(1000).springify()} 
+                  className="p-2 rounded-2xl w-full"
+                >
+                  <FormTextInput
+                    name='address'
+                    placeholder='Địa chỉ'
+                    control={customerControl}
+                  />
+                </Animated.View>
+              </>
+              :
+              <>
+                <Animated.View 
+                  entering={FadeInDown.duration(1000).springify()} 
+                  className="p-5 rounded-2xl w-full">
+                      <View className='flex flex-row'>
+                          {otp.map((digit, index) => (
+                            <TextInput
+                                  className='border rounded-md m-1 border-sky-800 opacity-70 h-12 w-12 text-center text-xl'
+                                  ref={(ref) => (inputRefs.current[index] = ref as TextInputProp)}
+                                  key={index}
+                                  maxLength={1}
+                                  keyboardType="numeric"
+                                  value={digit}
+                                  onChangeText={(value) => handleOtpChange(index, value)}
+                                  onKeyPress={({ nativeEvent  }) => {
+                                    handleKeyPress(nativeEvent, index)
+                                  }}
+                              />
+                          ))}
+                    </View>
+                </Animated.View>
+
+                <Animated.View className="w-full flex-row gap-3 mt-10" entering={FadeInDown.delay(600).duration(1000).springify()}>
+
+                  <TouchableOpacity className="flex-1 mt-20 bg-sky-400 p-3 rounded-2xl mb-3" onPress={() => {
+                    checkVerify();
+                  }}>
+                      <Text className="text-xl font-bold text-white text-center">Xác thực</Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    className="flex-1 mt-20 border-2 border-sky-400 p-3 rounded-2xl mb-3"
+                    onPress={handleSubmitPhoneNumber(onSubmit)}>
+                      <Text className="text-xl font-bold text-sky-400 text-center">Gửi lại OTP</Text>
+                  </TouchableOpacity>
+
+                </Animated.View>
+              </>
+            :
+            <>
+              <FormTextInput
+                name='phoneNumber'
+                placeholder='Số điện thoại'
+                control={phoneNumberControl}
+              />
 
             <Animated.View className="w-full" entering={FadeInDown.delay(600).duration(1000).springify()}>
-                <TouchableOpacity className="w-full mt-20 bg-sky-400 p-3 rounded-2xl mb-3" onPress={handleSubmit(onSubmit)}>
+                <TouchableOpacity
+                  className="w-full mt-20 bg-sky-400 p-3 rounded-2xl mb-3"
+                  onPress={handleSubmitPhoneNumber(onSubmit)}
+                >
                     <Text className="text-xl font-bold text-white text-center">Xác thực</Text>
                 </TouchableOpacity>
             </Animated.View>
-            <Animated.View 
-                    entering={FadeInDown.delay(600).duration(1000).springify()} 
-                    className="flex-row justify-center">
+            </>
+        }
+          <Animated.View 
+              entering={FadeInDown.delay(600).duration(1000).springify()} 
+              className="flex-row justify-center">
 
-                    <Text>Bạn đã có tài khoản? </Text>
-                    <TouchableOpacity onPress={()=> navigation.push('login')}>
-                        <Text className="text-sky-600">Đăng nhập</Text>
-                    </TouchableOpacity>
-                </Animated.View>                    
-        </View>
+              <Text>Bạn đã có tài khoản? </Text>
+              <TouchableOpacity onPress={()=> navigation.push('login')}>
+                  <Text className="text-sky-600">Đăng nhập</Text>
+              </TouchableOpacity>
+          </Animated.View>                    
       </View>
     </View>
-  )
+  </View>
+  </KeyboardAwareScrollView>
+)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  otpContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  input: {
-    height: 50,
-    width: 50,
-    borderWidth: 1,
-    borderRadius: 5,
-    textAlign: 'center',
-    fontSize: 20,
-  },
-  button: {
-    marginTop: 20,
-    backgroundColor: '#007BFF',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-  },
-});
